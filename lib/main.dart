88c0628b2aefb72576511e15419fa43a18c0d76d
user <ramshid.abc@gmail.com>
2024-08-22 13:40:42 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,18 @@ import 'package:coffee_ui/features/presentation/home_page/bloc/home_page_bloc.da
 import 'package:coffee_ui/features/presentation/quantity_page/bloc/quantity_page_bloc.dart';
 import 'package:coffee_ui/init_dependencies.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   await initDependencies();
   runApp(const MyApp());
 }
