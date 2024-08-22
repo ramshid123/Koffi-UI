cf21ed16f0f8cf2c2dde9d8a90c95cdee4f3f8c0
user <ramshid.abc@gmail.com>
2024-08-22 14:34:21 +0530

    updated

--- lib/core/constants/theme/palette.dart
+++ lib/core/constants/theme/palette.dart
@@ -4,7 +4,7 @@ class ColorConstants {
   static const textColor = Color(0xff4c4b46);
   static const backGroundColor = Color(0xfffdf7ed);
   static const liteContainerColor = Color(0xffefe8e3);
-  static const blackContainerColor = Color(0xff202120);
+  static const blackContainerColor = Color.fromARGB(255, 53, 53, 53);
   static const liteTextColor = Color(0xffcbc5bb);
   static const iconColor = Color(0xff000000);
   static const brownColor = Colors.brown;
